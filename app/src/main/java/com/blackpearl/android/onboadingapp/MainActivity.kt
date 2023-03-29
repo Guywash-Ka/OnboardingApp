d8dc0e8a23bf6db777c7d827c627f8f259c354b1
Guywash-Ka <rodvlasov2003@yandex.ru>
2023-03-29 17:55:32 +0300

    Added bottom navigation

--- app/src/main/java/com/blackpearl/android/onboadingapp/MainActivity.kt
+++ app/src/main/java/com/blackpearl/android/onboadingapp/MainActivity.kt
@@ -2,13 +2,13 @@ package com.blackpearl.android.onboadingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
 import android.view.MenuItem
-import androidx.activity.OnBackPressedDispatcher
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
@@ -26,6 +26,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val navigationView = findViewById<NavigationView>(R.id.nav_view)
         navigationView.setNavigationItemSelectedListener(this)
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        onBottomNavigationItemSelected(bottomNavigationView)
+
         val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
 
         drawerLayout.addDrawerListener(toggle)
@@ -40,19 +43,44 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.nav_home -> supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, HomeFragment()).commit()
-            R.id.nav_profile -> supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, ProfileFragment()).commit()
-            R.id.nav_quest -> supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, QuestFragment()).commit()
-            R.id.nav_register -> supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, RegisterFragment()).commit()
-            R.id.nav_logout -> supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, RegisterFragment()).commit()
+            R.id.nav_home -> loadFragment(HomeFragment())
+            R.id.nav_profile -> loadFragment(ProfileFragment())
+            R.id.nav_quest -> loadFragment(QuestFragment())
+            R.id.nav_register -> loadFragment(RegisterFragment())
+            R.id.nav_logout -> loadFragment(RegisterFragment())
         }
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    private fun onBottomNavigationItemSelected(bottomNavigationView: BottomNavigationView) {
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.nav_profile -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+                R.id.nav_challenges -> {
+                    loadFragment(ChallengeFragment())
+                    true
+                }
+                R.id.nav_calendar -> {
+                    loadFragment(CalendarFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container,fragment)
+        transaction.commit()
     }
 
     override fun onBackPressed() {
