92a1d1a1f64f1058fbefa1d017bfb31ede1a7e39
Guywash-Ka <rodvlasov2003@yandex.ru>
2023-03-31 17:21:40 +0300

    Fixed bottom nav

--- app/src/main/java/com/blackpearl/android/onboadingapp/MainActivity.kt
+++ app/src/main/java/com/blackpearl/android/onboadingapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.blackpearl.android.onboadingapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
 import com.blackpearl.android.onboadingapp.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var appBarConfiguration: AppBarConfiguration
@@ -18,29 +22,39 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.name_profile -> findNavController(R.id.nav_host_fragment).navigate(R.id.profile_fragment)
-                R.id.image_profile -> findNavController(R.id.nav_host_fragment).navigate(R.id.profile_fragment)
-                R.id.home_fragment -> findNavController(R.id.nav_host_fragment).navigate(R.id.home_fragment)
-                R.id.profile_fragment -> findNavController(R.id.nav_host_fragment).navigate(R.id.profile_fragment)
-                R.id.challenge_fragment -> findNavController(R.id.nav_host_fragment).navigate(R.id.challenge_fragment)
-                R.id.calendar_fragment -> findNavController(R.id.nav_host_fragment).navigate(R.id.calendar_fragment)
-            }
-            false
-        }
+
+
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
         binding.toolbar.setupWithNavController(navController, appBarConfiguration)
         binding.navView.setupWithNavController(navController)
-        binding.bottomNavigation.setOnItemSelectedListener(mOnNavigationItemSelectedListener)
+        binding.bottomNavigation.setupWithNavController(navController)
+
+//        val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+//            when (item.itemId) {
+//                R.id.name_profile -> findNavController(R.id.nav_host_fragment).navigate(R.id.profile_fragment)
+//                R.id.image_profile -> findNavController(R.id.nav_host_fragment).navigate(R.id.profile_fragment)
+//                R.id.home_fragment -> findNavController(R.id.nav_host_fragment).navigate(R.id.home_fragment)
+//                R.id.profile_fragment -> findNavController(R.id.nav_host_fragment).navigate(R.id.profile_fragment)
+//                R.id.challenge_fragment -> findNavController(R.id.nav_host_fragment).navigate(R.id.challenge_fragment)
+//                R.id.calendar_fragment -> findNavController(R.id.nav_host_fragment).navigate(R.id.calendar_fragment)
+//            }
+//            false
+//        }
+
+//        binding.bottomNavigation.setOnItemSelectedListener(mOnNavigationItemSelectedListener)
+
+
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
+
 
 }
